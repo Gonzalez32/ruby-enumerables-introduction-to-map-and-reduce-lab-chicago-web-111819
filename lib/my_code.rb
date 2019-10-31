cbcbbea5eed859ad9a5221763486883be8fb7e3b
Juan Gonzalez <3232rose@gmail.com>
2019-10-31 17:57:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,10 +1,10 @@
 # My Code here....
 def map_to_negativize(array)
-    myArr = []
+    myArray = []
     count = 0
     while count < array.length do
         myArr.push(array[count] * - 1)
         count += 1
     end
-    return myArr
+    return myArray
 end
